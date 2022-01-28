44b432432c7686249bd7ad1e831ad32ab31f56f2
Mark Jongkind <mark@backscreen.nl>
2022-01-28 11:10:52 +0100

    Test

--- knockout-datatable.js
+++ knockout-datatable.js
@@ -1,6 +1,8 @@
 (function() {
   var indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
+  console.log('Test knockout-datatable change');
+
   window.DataTable = (function() {
     var primitiveCompare, pureComputed;
 
